e8a1549cdbcb27442bfda6124baa9c295c61360b
ducthanh160 <ducthanh160103@gmail.com>
2022-12-27 20:53:23 +0700

    FlatList when there is no id or key

--- App.js
+++ App.js
@@ -27,19 +27,19 @@ import {
 
 export default function App() {
   const [Items, setItems] = useState([
-    {id: '1', name: 'item 1'},
-    {id: '2', name: 'item 2'},
-    {id: '3', name: 'item 3'},
-    {id: '4', name: 'item 4'},
-    {id: '5', name: 'item 5'},
-    {id: '6', name: 'item 6'},
-    {id: '7', name: 'item 7'},
-    {id: '8', name: 'item 8'},
-    {id: '9', name: 'item 9'},
-    {id: '10', name: 'item 10'},
-    {id: '11', name: 'item 11'},
-    {id: '12', name: 'item 12'},
-    {id: '13', name: 'item 13'},
+    {name: 'item 1'},
+    {name: 'item 3'},
+    {name: 'item 2'},
+    {name: 'item 4'},
+    {name: 'item 5'},
+    {name: 'item 6'},
+    {name: 'item 7'},
+    {name: 'item 8'},
+    {name: 'item 9'},
+    {name: 'item 10'},
+    {name: 'item 11'},
+    {name: 'item 12'},
+    {name: 'item 13'},
   ])
   const [Refreshing, setRefreshing] = useState(false);
 
@@ -51,7 +51,7 @@ export default function App() {
 
   return (
     <FlatList
-      keyExtractor={(item)=>item.id}
+      keyExtractor={(item,index)=> index.toString()}
       data={Items}
       renderItem={({item}) => (
         <View style={styles.item}>
